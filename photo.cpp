4913b9b8d81ab9cad57944826258dc8a67c9b9f7
Krisjanis Veinbahs <krisjanisv@scandiweb.com>
2019-03-02 13:48:29 +0200

    Some more code that was unsaved

---
+++ photo.cpp
@@ -0,0 +1,25 @@
+//Constants
+const int pResistor = A0; // Photoresistor at Arduino analog pin A0
+const int ledPin=9;       // Led pin at Arduino pin 9
+
+//Variables
+int value;				  // Store value from photoresistor (0-1023)
+
+void setup(){
+ pinMode(ledPin, OUTPUT);  // Set lepPin - 9 pin as an output
+ pinMode(pResistor, INPUT);// Set pResistor - A0 pin as an input (optional)
+}
+
+void loop(){
+  value = analogRead(pResistor);
+  
+  //You can change value "25"
+  if (value > 25){
+    digitalWrite(ledPin, LOW);  //Turn led off
+  }
+  else{
+    digitalWrite(ledPin, HIGH); //Turn led on
+  }
+
+  delay(500); //Small delay
+}
